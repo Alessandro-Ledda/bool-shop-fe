2e7eef8cca2cfa327efe5313fdbb04312ec43ec7
Alessandro Ledda <aleledda75@icloud.com>
2026-03-09 16:47:22 +0100

    feat detail

---
+++ src/components/CardDetail.jsx
@@ -0,0 +1,4 @@
+
+export default function CardDetail({ cardProp }) {
+    return
+}
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,43 @@
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+//importo componente di ref per dettaglio
+import CardDetail from "../components/CardDetail";
+//salvo endpoint in var
+const endPoint = "http://localhost:3000/api/products"
 
-
+//set funzione per il dettaglio
 export default function ProductDetail() {
+
+    //recupero id di ref attraverso params
+    const { id } = useParams();
+
+    //setto var di stato per gestione prodotto
+    const [product, setProduct] = useState({});
+
+    //setto funzione per gestione chiamata alla show del db
+    function fetchProduct() {
+        //chiamata
+        axios.get(`${endPoint}/${id}`)
+            .then(res => { setProduct(res.data); })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally()
+    }
+
+    //richiamo effect per il controllo su effetti collaterali
+    useEffect(fetchProduct, []);
+
+    return (
+        <header id="card-product">
+            <div className="page-container">
+                <img src={product.image} alt={product.name} />
+            </div>
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <Link className="btn" to="/">Ritorna alla Home</Link>
+        </header>
+    )
 
 }
