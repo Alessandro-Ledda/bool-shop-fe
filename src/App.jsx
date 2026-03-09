362d4d2974c7b3bfc300bb8dcd5c9c0115669329
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-09 11:13:08 +0100

    feat : added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routers, Route } from "react-router-dom"
+import DefaultLayout from "./layout/DefaultLayout"
+import Homepage from "./pages/Homepage"
+import ProductDetail from "./pages/ProductDetail"
 
 
 function App() {
 
-  return
+  return (
+    <BrowserRouter>
+      <Routers>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="products/:slug" element={<ProductDetail />} />
+        </Route>
+      </Routers>
+    </BrowserRouter>
+  )
 
 
 
