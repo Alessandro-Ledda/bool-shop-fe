c84e68d648729db7ea7d94b9e052bbc397a2eee3
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-09 16:03:30 +0100

    created context

---
+++ src/contexts/ApiProvider.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import axios from "axios";
+
+const endpoint = import.meta.env.VITE_APP_URL;
+const ApiContext = createContext();
+
+export function ApiProvider({ children }) {
+
+    const [products, setProducts] = useState([]);
+
+    function fetchProduct() {
+
+        axios.get(`${endpoint}api/products`)
+            .then(res => {
+                setProducts(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally()
+    }
+
+
+    useEffect(fetchProduct, [])
+
+
+    return (
+        <ApiContext.Provider value={{ products, setProducts }}>
+            {children}
+        </ApiContext.Provider>
+    )
+}
+
+export function useApi() {
+    const context = ApiContext;
+    return context;
+}
