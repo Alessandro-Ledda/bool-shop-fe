1f9732c0ad52156747f14c7f551026482a0b8b36
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-09 13:02:24 +0100

    fix image-prop

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ export default function ProductCard({ productProp }) {
     return (
         <div className="card border border-3 col-4 border-primary-subtle bg-secondary-subtle">
             <figure className="my-figure">
-                <img src={productProp.image} alt={productProp.name} />
+                <img src={productProp.image_url} alt={productProp.name} />
             </figure>
             <figcaption>
                 <div>{productProp.price}</div>
