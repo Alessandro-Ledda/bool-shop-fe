73674b4c2ba47e55c391e1c915e124c3c007e0d4
Alessandro Ledda <aleledda75@icloud.com>
2026-03-09 11:15:01 +0100

    feat im mainheader

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,5 +1,22 @@
+// import link
+import { Link } from "react-router-dom"
+
 function MainHeader() {
-    return
+    return (
+        <header>
+            <Link className="logo">BoolShop</Link>
+            <nav className="navbar">
+                <div className="container">
+                    <div className="navigation">
+                        <ul>
+                            <li><Link>homePage</Link></li>
+                            <li><Link>All product</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default MainHeader
