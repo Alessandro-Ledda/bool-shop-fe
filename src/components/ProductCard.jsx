a72a1e817031e78f3e0ee331876d8fc4dd0fcf60
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-09 12:26:03 +0100

    fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,14 @@
 
 export default function ProductCard({ productProp }) {
-    <div className="card border border-3 col-4 border-primary-subtle bg-secondary-subtle">
-        <figure className="my-figure">
-            <img src={productProp.image} alt={productProp.name} />
-        </figure>
-        <figcaption>
-            <div>{productProp.price}</div>
-            <button className="btn">Aggiungi al carrello</button>
-        </figcaption>
-    </div>
+    return (
+        <div className="card border border-3 col-4 border-primary-subtle bg-secondary-subtle">
+            <figure className="my-figure">
+                <img src={productProp.image} alt={productProp.name} />
+            </figure>
+            <figcaption>
+                <div>{productProp.price}</div>
+                <button className="btn">Aggiungi al carrello</button>
+            </figcaption>
+        </div>
+    )
 }
