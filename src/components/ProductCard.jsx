bd3c03fbadfc37127f2c3081d918ef44859da5d3
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-09 11:41:54 +0100

    feat: added ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+
+export default function ProductCard() {
+    <div className="card border border-3 col-4 border-primary-subtle bg-secondary-subtle">
+        <figure className="my-figure">
+            <img />
+        </figure>
+        <figcaption>
+            <div>price</div>
+            <button className="btn">Aggiungi al carrello</button>
+        </figcaption>
+    </div>
+}
