483911778574b5c0f4333f2e57ead144b3b41a31
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-10 11:14:39 +0100

    banner

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,19 +1,32 @@
 // import link
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function MainHeader() {
-    return (
-        <header>
-            <Link className="logo">BoolShop</Link>
-            <nav className="navbar">
-                <ul>
-                    <li><Link to={"/"}>homePage</Link></li>
-                    <li><Link>WhishList</Link></li>
-                    <li><Link>Cart</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <>
+      {/* Banner  */}
+      <div className="py-2 text-center banner">
+        <span className="fw-bold text-dark">SPEDIZIONI GRATUITE</span>
+        <span className="text-dark ms-1">SU ORDINI SOPRA I 100&euro;</span>
+      </div>
+      <header>
+        <Link className="logo">BoolShop</Link>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to={"/"}>homePage</Link>
+            </li>
+            <li>
+              <Link>WhishList</Link>
+            </li>
+            <li>
+              <Link>Cart</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
-export default MainHeader
+export default MainHeader;
