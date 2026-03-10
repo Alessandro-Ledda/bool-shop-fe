fdb7e4d241a706468d451289f90a44de0508a9f4
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-10 17:13:59 +0100

    fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,6 @@
 import { Link } from "react-router-dom";
 
 export default function ProductCard({ productProp }) {
-<<<<<<< HEAD
-    return (
-        <div className="card-list card ">
-            <Link to={`/products/${productProp.slug}`}>
-                <figure className="my-figure">
-                    <img className="img-card-list" src={productProp.image_url} alt={productProp.name} />
-                </figure>
-                <figcaption>
-                    <div className="product-title">{productProp.name}</div>
-                </figcaption>
-            </Link>
-            <div className="product-price">{productProp.price}</div>
-            <button className="btn btn-primary">Aggiungi al carrello</button>
-
-        </div >
-    )
-}
-=======
   return (
     <Link to={`/products/${productProp.slug}`} className="card-link">
       <img
@@ -34,4 +16,3 @@ export default function ProductCard({ productProp }) {
     </Link>
   );
 }
->>>>>>> main-test
