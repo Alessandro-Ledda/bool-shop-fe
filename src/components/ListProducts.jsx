8bd16bcd826625280c09c72d6142f22a53a27bc2
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-10 15:02:07 +0100

    add margini alle cards

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,38 +1,32 @@
 // import card del singolo prodotto
 import ProductCard from "./ProductCard";
 
-
-
 import { useApi } from "../contexts/ApiProvider";
 
 function ListProducts() {
+  const { products } = useApi();
 
-    const { products } = useApi();
+  // funzione per renderizzare i prodotti
+  function renderProducts() {
+    return products.map((product) => {
+      return (
+        <div className="wrap" key={product.id}>
+          <ProductCard productProp={product} />
+        </div>
+      );
+    });
+  }
 
+  if (!products.length) return <div>Nessun prodotto</div>;
 
-    // funzione per renderizzare i prodotti
-    function renderProducts() {
-        return products.map(product => {
-            return (
-                <div className="wrap" key={product.id}>
-                    <ProductCard productProp={product} />
-                </div>
-            )
-        })
-    }
-
-    if (!products.length) return <div>Nessun prodotto</div>;
-
-    return (
-        <>
-            <h2>Product List</h2>
-
-            <div className="container">
-                {renderProducts()}
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      <div className="container mt-5">
+        <h2 className="mb-5">Product List</h2>
+        {renderProducts()}
+      </div>
+    </>
+  );
 }
 
-export default ListProducts
+export default ListProducts;
