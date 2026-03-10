43cc0fd1b920412a43e34630be0781ea916f2521
Alessandro Ledda <aleledda75@icloud.com>
2026-03-10 14:39:50 +0100

    feat

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -2,52 +2,64 @@ import { Carousel } from "react-bootstrap";
 
 function HeroCarousel() {
   return (
-    <Carousel>
+    <Carousel className="hero-carousel">
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../public/hero1.jpg"
+          src="hero1.jpg"
           alt="Prima slide"
         />
         <Carousel.Caption>
-          <h3>Promo Estate</h3>
-          <p>Scopri la nostra collezione di attrezzi fitness</p>
+          <h3>Lat Machine Professionale</h3>
+          <p>
+            Massima efficacia per dorsali e spalle, occupando pochissimo spazio
+            a casa.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../public/hero2.webp"
+          src="hero2.webp"
           alt="Seconda slide"
         />
         <Carousel.Caption>
-          <h3>Offerta Speciale</h3>
-          <p>Spedizione gratuita su ordini sopra i 50&euro;</p>
+          <h3>Home Gym Compatta</h3>
+          <p>
+            Tutti gli esercizi in un unico macchinario, massimi risultati con
+            minima ingombro.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../public/hero3.jpg"
+          src="hero3.jpg"
           alt="Terza slide"
         />
         <Carousel.Caption>
-          <h3>Nuovi Arrivi</h3>
-          <p>Attrezzi di qualità per la tua palestra in casa</p>
+          <h3>Set di Manubri Regolabili</h3>
+          <p>
+            Allenamento completo in poco spazio, qualità professionale per ogni
+            esercizio.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../public/hero4.webp"
+          src="hero4.webp"
           alt="Prima slide"
         />
         <Carousel.Caption>
-          <h3>Promo Estate</h3>
-          <p>Scopri la nostra collezione di attrezzi fitness</p>
+          <h3>Bilanciere Professionale per Squat</h3>
+          <p>
+            Resistente e compatto, ideale per massimizzare i risultati senza
+            sacrificare spazio.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
