56dbc5243cd1afeb6c5778ecde33dcef51dc86ad
Alessandro Ledda <aleledda75@icloud.com>
2026-03-12 10:40:45 +0100

    fix img in cartpage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,8 +1,12 @@
 import { useCart } from "../contexts/CartContext";
-import ProductCardCart from "../components/ProductCardCart";
+import { useApi } from "../contexts/ApiProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 function CartPage() {
     const { cart, removeFromCart } = useCart();
+    const { products } = useApi();
 
     const total = cart.reduce(
         (acc, item) => acc + item.price * item.quantity,
@@ -11,19 +15,42 @@ function CartPage() {
 
     return (
         <div>
-            <h1 className="title">Carrello</h1>
+            <h1 className="title">Riepilogo Carrello</h1>
 
-            {cart.map((item) => (
-                <div key={item.id}>
-                    <h3>{item.name}</h3>
-                    <p>Quantità: {item.quantity}</p>
-                    <p>Prezzo: {item.price}€</p>
+            {cart.length === 0 && <p>Il carrello è vuoto</p>}
 
-                    <button onClick={() => removeFromCart(item.id)}>
-                        Rimuovi
-                    </button>
-                </div>
-            ))}
+            {cart.map((item) => {
+                // trova il prodotto completo dal backend
+                const product = products.find((p) => p.id === item.id);
+
+                return (
+                    <ul key={item.id} className="cart-item">
+                        {product && product.image && (
+                            <li>
+                                <img
+                                    src={product.image_url}
+                                    alt={item.name}
+                                    width="80"
+                                />
+                            </li>
+                        )}
+                        <li>{item.name}</li>
+                        <li>Quantità: {item.quantity}</li>
+                        <li>Prezzo: {item.price}€</li>
+
+
+
+                        <li>
+                            <button
+                                onClick={() => removeFromCart(item.id)}
+                                className="btn-font-aw"
+                            >
+                                <FontAwesomeIcon icon={faTrash} color="red" />
+                            </button>
+                        </li>
+                    </ul>
+                );
+            })}
 
             <h2>Totale: {total}€</h2>
         </div>
