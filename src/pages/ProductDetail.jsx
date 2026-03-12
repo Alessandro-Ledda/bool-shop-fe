6884e5a5a66eaeef53cbb87079473e32757af40f
marcotristani <marco.tristani@hotmail.it>
2026-03-12 11:54:14 +0100

    definito form, salvato momentaneamente su pagina dettaglio prodotto

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -6,34 +6,41 @@ import CardDetail from "../components/CardDetail";
 //salvo endpoint in var
 const endpoint = import.meta.env.VITE_APP_URL;
 
+//importo form prova
+import FormCheckout from "../components/FormCheckout";
+
 //set funzione per il dettaglio
 export default function ProductDetail() {
+  //recupero id di ref attraverso params
+  const { slug } = useParams();
 
-    //recupero id di ref attraverso params
-    const { slug } = useParams();
+  //setto var di stato per gestione prodotto
+  const [product, setProduct] = useState({});
 
-    //setto var di stato per gestione prodotto
-    const [product, setProduct] = useState({});
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  //setto funzione per gestione chiamata alla show del db
+  function fetchProduct() {
+    //chiamata
+    axios
+      .get(`${endpoint}api/products/${slug}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.log("Prodotto non trovato", err);
+        navigate("/notfound");
+      })
+      .finally();
+  }
 
-    //setto funzione per gestione chiamata alla show del db
-    function fetchProduct() {
-        //chiamata
-        axios.get(`${endpoint}api/products/${slug}`)
-            .then(res => { setProduct(res.data); })
-            .catch(err => {
-                console.log("Prodotto non trovato", err);
-                navigate("/notfound")
-            })
-            .finally()
-    }
+  //richiamo effect per il controllo su effetti collaterali
+  useEffect(fetchProduct, [slug]);
 
-    //richiamo effect per il controllo su effetti collaterali
-    useEffect(fetchProduct, [slug]);
-
-    return (
-        <CardDetail product={product} />
-    )
-
+  return (
+    <>
+      <CardDetail product={product} />
+      <FormCheckout />
+    </>
+  );
 }
