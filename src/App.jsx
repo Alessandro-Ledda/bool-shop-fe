03616e3a3c5d6c92c06821a1d9c41652c80bfa5b
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-12 10:59:48 +0100

    fix cart

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,19 @@ function App() {
   return (
     <BrowserRouter>
       <ApiProvider>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="products/:slug" element={<ProductDetail />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/wishlist" element={<WishList />} />
-            <Route path='/notfound' element={<ErrorPage />} />
-            <Route path='*' element={<ErrorPage />} />
-          </Route>
-        </Routes>
-
+        <CartProvider>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="products/:slug" element={<ProductDetail />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/wishlist" element={<WishList />} />
+              <Route path='/notfound' element={<ErrorPage />} />
+              <Route path='*' element={<ErrorPage />} />
+            </Route>
+          </Routes>
+        </CartProvider>
       </ApiProvider>
     </BrowserRouter>
   )
