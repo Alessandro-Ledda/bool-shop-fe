8ab83e7e2cb4783cd154aa9b73b3b9ff0955c92a
marcotristani <marco.tristani@hotmail.it>
2026-03-12 14:58:57 +0100

    fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
+
 const Loader = () => {
   return (
     <div className="overlay-loader">
-      <div className="spinner-border text-primary" role="status"></div>
+      <div className="spinner-loader" role="status">
+        <FontAwesomeIcon icon={faDumbbell} className="text-warning" />
+      </div>
     </div>
   );
 };
