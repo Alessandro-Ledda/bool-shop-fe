73db3cb102bb49f71f6f706b47266ff095cce001
marcotristani <marco.tristani@hotmail.it>
2026-03-13 12:49:14 +0100

    sistemato navigate in checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../contexts/CartContext";
+import { useNavigate } from "react-router-dom";
 //importo form
 import FormCheckout from "../components/FormCheckout";
 import CartPreview from "../components/CartPreview";
@@ -7,6 +8,7 @@ import CartPage from "./CartPage";
 
 function CheckoutPage() {
   const { cart, clearCart } = useCart();
+  const navigate = useNavigate();
   if (cart.length === 0) {
     return (
       <div className="container d-flex justify-content-center align-items-center container-order-success ">
@@ -32,16 +34,18 @@ function CheckoutPage() {
         </Link>
       </div>
       <CartPreview />
-      <Link>
-        <button className="search-button mb-4"
-          onClick={() => {
-            if (window.confirm("Sei sicuro di voler svuotare il tuo carrello?")) {
-              clearCart();
-            }
-          }}
-        >
-          Svuota Carrello</button>
-      </Link>
+      <button
+        className="search-button mb-4"
+        onClick={() => {
+          if (window.confirm("Sei sicuro di voler svuotare il tuo carrello?")) {
+            clearCart();
+            navigate("/");
+          }
+        }}
+      >
+        Svuota Carrello
+      </button>
+
       <FormCheckout />
     </div>
   );
