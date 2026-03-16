30e1951604c6c9aed3d39eeafa5f87fec0377766
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-03-16 19:11:05 +0100

    fix: fixed wishlist logic with context WishilistContext functions

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,40 +1,53 @@
-import { useApi } from "../contexts/ApiProvider"
-import { useWishlist } from "../contexts/WishlistContext"
+import { Link } from "react-router-dom";
+import { useApi } from "../contexts/ApiProvider";
+import { useWishlist } from "../contexts/WishlistContext";
 
 function WishList() {
 
-    // var per consumare il contesto
     const { products } = useApi();
     const { wishlist, removeFromWishlist } = useWishlist();
 
-    if (!products) return <p>Caricamento prodotti...</p>
+    if (!products) return <p>Caricamento prodotti...</p>;
 
-    const wishListProducts = products?.filter(product =>
-        (wishlist || []).includes(product.id)
+    const wishListProducts = products.filter(product =>
+        wishlist.includes(product.id)
     );
 
     return (
-        <>
-            <div className="container-wishlist">
-                <div className="content-wishlist">
-                    <h1 className="title">Lista Desideri</h1>
+        <div className="container-wishlist">
+            <div className="content-wishlist">
+
+                <h1 className="title">Lista Desideri</h1>
+
+                {wishListProducts.length === 0 ? (
+
                     <p>La wishlist è vuota</p>
-                    {wishListProducts.map(product => (
-                        <div key={product.id}>
-                            <h3>{product.name}</h3>
+
+                ) : (
+
+                    wishListProducts.map(product => (
+                        <div key={product.id} className="wishlist-item">
+
+                            <Link to={`/products/${product.slug}`}>
+                                <h3>{product.name}</h3>
+                            </Link>
+
                             <p>{product.price}€</p>
 
                             <button
                                 onClick={() => removeFromWishlist(product.id)}
-                            >Rimuovi</button>
+                            >
+                                Rimuovi
+                            </button>
 
                         </div>
-                    ))}
-                </div>
-            </div>
+                    ))
 
-        </>
-    )
+                )}
+
+            </div>
+        </div>
+    );
 }
 
-export default WishList
+export default WishList;
